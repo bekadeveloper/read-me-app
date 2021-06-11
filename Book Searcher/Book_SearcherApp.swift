c5c7368514a5468f5596e41cdd30bf07555c2281
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-06-11 16:50:40 +0500

    Initial Commit

---
+++ Book Searcher/Book_SearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_SearcherApp.swift
+//  Book Searcher
+//
+//  Created by Begzod on 11/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Book_SearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Book Searcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Book Searcher
+//
+//  Created by Begzod on 11/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
